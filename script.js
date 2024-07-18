a65a6a67d1b510ffe75a294fcaf6aa6086084eb5
saloxiddin97 <salohiddinshamsidinov0101@gmail.com>
2024-07-18 11:50:40 +0500

    asd

--- script.js
+++ script.js
@@ -3,10 +3,24 @@ let lsit=document.querySelector('ol')
  
 btn.addEventListener('click',()=>{
     let inp=document.querySelector('input').value
-
+ if(inp ===''){
+    alert('write something')
+ }else{
     let newli=document.createElement('li')
     lsit.appendChild(newli)
     newli.textContent=inp
 
 
+let newSpan =document.createElement('span')
+newSpan.innerHTML="\u00d7";
+newli.appendChild(newSpan)
+
+ }
+})
+let ul=document.querySelector('ol')
+
+ul.addEventListener('click',(e)=>{
+    if(e.target.tagName ==='SPAN'){
+e.target.parentElement.remove()
+    }
 })
